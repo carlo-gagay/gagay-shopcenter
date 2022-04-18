b2a96dd0af9c0bbea29b898c64b643a6f0a05d60
carlo-gagay <tabayagcarlo94@gmail.com>
2022-04-18 21:06:43 +0800

    first commit

---
+++ src/test/java/com/gagay/shopcenter/ShopcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagay.shopcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
